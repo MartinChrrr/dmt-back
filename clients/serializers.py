67ff3b5e747382ea1dc98078e4901bbc9bfc5d9b
martin <martin_charrier@yahoo.fr>
2026-02-05 11:11:13 +0100

    add tests for adresses and clients

--- clients/serializers.py
+++ clients/serializers.py
@@ -1,21 +1,42 @@
 from rest_framework import serializers
 from django.apps import apps
 
-
+class AdresseNestedSerializer(serializers.ModelSerializer):
+    """Serializer pour les adresses imbriquées dans ClientSerializer"""
+    class Meta:
+        model = apps.get_model('clients', 'Adresse')
+        fields = ['id', 'type', 'ligne1', 'ligne2', 'code_postal', 'ville', 'pays', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+        
 class AdresseSerializer(serializers.ModelSerializer):
     class Meta:
         model = apps.get_model('clients', 'Adresse')
         fields = '__all__'
-        read_only_fields = ['id', 'client', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
 
 class ClientSerializer(serializers.ModelSerializer):
-    adresses = AdresseSerializer(many=True, required=False)
+    adresses = AdresseNestedSerializer(many=True, required=False)
 
     class Meta:
         model = apps.get_model('clients', 'Client')
         fields = '__all__'
         read_only_fields = ['utilisateur']
+
+    def validate_raison_sociale(self, value):
+        """Vérifie l'unicité par utilisateur"""
+        request = self.context['request']
+        queryset = self.Meta.model.objects.filter(
+            utilisateur=request.user,
+            raison_sociale=value
+        )
+        if self.instance:
+            queryset = queryset.exclude(pk=self.instance.pk)
+        if queryset.exists():
+            raise serializers.ValidationError(
+                "Vous avez déjà un client avec cette raison sociale."
+            )
+        return value
 
     def create(self, validated_data):
         adresses_data = validated_data.pop('adresses', [])
