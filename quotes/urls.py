1fdafe989eba160c0ff0e2d9abb21bcaf6982097
KohakuC <60190850+KohakuC@users.noreply.github.com>
2026-02-05 14:31:23 +0100

    ulrs for quotes

---
+++ quotes/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DevisViewSet, LigneDevisViewSet, HistoriqueDevisViewSet
+
+router = DefaultRouter()
+
+router.register(r'devis', DevisViewSet, basename='devis')
+router.register(r'lignes-devis', LigneDevisViewSet, basename='ligne-devis')
+router.register(r'historique-devis', HistoriqueDevisViewSet, basename='historique-devis')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
