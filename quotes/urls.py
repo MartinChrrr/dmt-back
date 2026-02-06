5b624ef71bc4c68aaf9f6f979bf6e43f93c56067
KohakuC <60190850+KohakuC@users.noreply.github.com>
2026-02-06 11:45:29 +0100

    ligne-devis and historique-devis commented

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,9 +4,9 @@ from .views import DevisViewSet, LigneDevisViewSet, HistoriqueDevisViewSet
 
 router = DefaultRouter()
 
-router.register(r'devis', DevisViewSet, basename='devis')
-router.register(r'lignes-devis', LigneDevisViewSet, basename='ligne-devis')
-router.register(r'historique-devis', HistoriqueDevisViewSet, basename='historique-devis')
+router.register('', DevisViewSet, basename='devis')
+# router.register(r'lignes-devis', LigneDevisViewSet, basename='ligne-devis')
+# router.register(r'historique-devis', HistoriqueDevisViewSet, basename='historique-devis')
 
 urlpatterns = [
     path('', include(router.urls)),
